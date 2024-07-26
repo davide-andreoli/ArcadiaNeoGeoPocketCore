11846fc1b00fe2e3ea5dd89e1bb2cdcc79609582
Davide Andreoli <davide.andreoli@icloud.com>
2024-07-26 18:06:12 +0200

    Initial Commit

---
+++ ArcadiaNeoGeoPocketCore/ArcadiaNeoGeoPocketCore.h
@@ -0,0 +1,18 @@
+//
+//  ArcadiaNeoGeoPocketCore.h
+//  ArcadiaNeoGeoPocketCore
+//
+//  Created by Davide Andreoli on 26/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArcadiaNeoGeoPocketCore.
+FOUNDATION_EXPORT double ArcadiaNeoGeoPocketCoreVersionNumber;
+
+//! Project version string for ArcadiaNeoGeoPocketCore.
+FOUNDATION_EXPORT const unsigned char ArcadiaNeoGeoPocketCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArcadiaNeoGeoPocketCore/PublicHeader.h>
+
+
